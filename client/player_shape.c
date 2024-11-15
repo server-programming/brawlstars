90c7ed34632dea1673c94e3a41fce316ca4ccce6
Lunarian0928 <thanks092834@gmail.com>
2024-11-16 08:55:45 +0900

    player_shape 관련 주석 처리 완료

--- client/player_shape.c
+++ client/player_shape.c
@@ -3,6 +3,7 @@
 #include <time.h>
 
 PlayerShape* get_player_shape() {
+    // 플레이어의 스킨(모양) 리스트를 초기화
     static PlayerShape player = {
         L"▄︻字═━──",
         L"︻̷═━一",
@@ -16,24 +17,27 @@ PlayerShape* get_player_shape() {
         L"︻̷═━一",
         L"︻╦══╤─",
         L"▄︻▇▇〓",
-	L"@(^ㅇ ^)==@",
-	L"(ง •̀_•́)ง",
-	L"╭( ･ㅂ･)و",
-	L"╭( ･ㅂ･)ว",
+    	L"@(^ㅇ ^)==@",
+	    L"(ง •̀_•́)ง",
+	    L"╭( ･ㅂ･)و",
+	    L"╭( ･ㅂ･)ว",
     };
 
-    // 시드 설정 (한번만 실행되어야 함)
+    // 시드 설정 (현재 시간 기준)
     srand(time(NULL));
 
     // 모양들을 랜덤하게 섞기
     for (int i = 0; i < MAX_SHAPES; i++) {
-        int j = rand() % MAX_SHAPES;
+        // 0 ~ MAX-SHAPES-1 사이의 난수를 설정
+        int j = rand() % MAX_SHAPES; 
+
         // 모양을 서로 교환 (Fisher-Yates Shuffle)
         wchar_t* temp = player.shapes[i];
         player.shapes[i] = player.shapes[j];
         player.shapes[j] = temp;
     }
-
+    
+    // 플레이어 모양 데이터를 변환
     return &player;
 }
 
--- client/player_shape.h
+++ client/player_shape.h
@@ -3,12 +3,16 @@
 
 #include <wchar.h>
 
+// 플레이어가 가질 수 있는 스킨(모양)의 개수
 #define MAX_SHAPES 18
 
+// PlayerShape 구조체 정의
 typedef struct {
+    // 와이드 문자열 포인터 배열로, 각 요소는 플레이어 스킨(모양)
     wchar_t* shapes[MAX_SHAPES];
 } PlayerShape;
 
+// 플레이어 모양(스킨)을 반환하는 함수 선언
 PlayerShape* get_player_shape();
 
 #endif
