c45cabafe831b3bcee469669ddc012b6da6dec8c
mega-317 <mega31712@gmail.com>
2024-11-25 22:27:38 +0900

    서버와 클라이언트간 통신에 사용하는 함수를 connect_to_client.c 파일로 분할함

---
+++ server/connect_to_client.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+
+int connect_to_client(int ns, int cur_client_num, char *buf, int flag) {
+	int network_status;
+	char buf1[50];
+	char buf2[200];
+
+	if (flag == 1) {
+		strcpy(buf1, buf);
+		network_status = send(ns, buf1, sizeof(buf1), 0);
+	}
+
+	if (flag == 2) {
+		strcpy(buf2, buf);
+		network_status = send(ns, buf2, sizeof(buf2), 0);
+	}
+
+	if (network_status == -1 || network_status == 0) {
+		return 0;
+	} else {
+		return 1;
+	}
+}
--- server/main.c
+++ server/main.c
@@ -220,26 +220,4 @@ int main() {
 	return 0;
 }
 
-int connect_to_client(int ns, int cur_client_num, char *buf, int flag) {
-	int network_status;
-	char buf1[50];
-	char buf2[200];
-
-	if (flag == 1) {
-		strcpy(buf1, buf);
-		network_status = send(ns, buf1, sizeof(buf1), 0);
-	}
-
-	if (flag == 2) {
-		strcpy(buf2, buf);
-		network_status = send(ns, buf2, sizeof(buf2), 0);
-	}
-
-	if (network_status == -1 || network_status == 0) {
-		return 0;
-	} else {
-		return 1;
-	}
-}
-
 
