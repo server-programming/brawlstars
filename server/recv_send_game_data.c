fcf7e0078ac582a39bdce98ba86bae1d4b65ff60
mega-317 <mega31712@gmail.com>
2024-11-27 22:36:14 +0900

    게임에서의 클라이언트와 서버의 통신을 recv_send_game_data 함수로 분할하였음. 그리고 이제 접속하지 않은 클라이언트들은 화면에서 보이지 않음.

---
+++ server/recv_send_game_data.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+#define PLAYER 4
+
+typedef struct player {
+        int x;
+        int y;
+        int skin;
+	int hp;
+	int is_dead;
+} player;
+
+typedef struct network_player {
+	int *ns;
+	struct player *players;
+	int cur_client;
+} network_player;
+
+int connect_to_client(int ns, int cur_client_num, char *buf, int flag);
+
+int recv_send_game_data(network_player *np, char *buf, int cur_client_num) {
+	char buf1[50];
+	char player_pos[1024];
+	int client_x, client_y, client_skin, client_hp, client_is_dead;
+	sscanf(buf, "ACCESS_TO_GAME,x=%d,y=%d,skin=%d,hp=%d,is_dead=%d\n",
+		&client_x, &client_y, &client_skin, &client_hp, &client_is_dead);
+
+	np->players[cur_client_num].x = client_x;
+	np->players[cur_client_num].y = client_y;
+	np->players[cur_client_num].skin = client_skin;
+	np->players[cur_client_num].hp = client_hp;
+	np->players[cur_client_num].is_dead = client_is_dead;
+
+	memset(player_pos, '\0', sizeof(player_pos));
+	for(int i=0; i<PLAYER; i++) {
+		memset(buf, '\0', sizeof(buf));
+		if (np->ns[i] > 0) {
+			sprintf(buf, "%d,x=%d,y=%d,skin=%d,hp=%d,is_dead=%d\n",
+				i, np->players[i].x, np->players[i].y, np->players[i].skin,
+				np->players[i].hp, np->players[i].is_dead);
+			strcat(player_pos, buf);
+		} else {
+			sprintf(buf, "%d,x=-10,y=-10,skin=0,hp=0,is_dead=0\n", i);
+		}
+	}
+
+	printf("%s\n", player_pos);
+
+	if (connect_to_client(np->ns[cur_client_num], cur_client_num, player_pos, 2) == 0) {
+		return 0;
+	}
+
+	return 1;
+}
